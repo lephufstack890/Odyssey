1ee9b04dd473232b5c705095487f91ece52b5e37
Le Phu <tui.hrv@gmail.com>
2024-03-01 23:00:42 +0700

    fix home v2

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,11 +3,13 @@ import styles from './Home.module.scss';
 import { Link } from 'react-router-dom';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import { useState } from 'react';
+import startedVideo from './../../assets/video/home/started.mp4';
 
 const cx = classNames.bind(styles);
 
@@ -115,6 +117,41 @@ function Home() {
                     <div className={cx('main-page__odyssey-content')}>
                         <h3>Why Choose Odyssey</h3>
                         <p>Our team comprises individuals who have undergone rigorous training and education at world-class universities. Our objective is to deliver creative and effective software solutions, enabling you to transform your aspirations into tangible outcomes.</p>
+                    </div>
+                    <div className={cx('main-page__odyssey-actions', 'container')}>
+                        <div className={cx('d-flex', 'align-items-center')}>
+                            <div className={cx('main-page__odyssey-actions-item')}>
+                                <span>Our Partners</span>
+                            </div>
+                            <div className={cx('main-page__odyssey-actions-item')}>
+                                <img src='https://static.wixstatic.com/media/c794a2_d951ecbf103042f8bc74eaeeb369d448~mv2.png/v1/fill/w_170,h_50,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/logo-microsoft-404px-grey.png' alt='' />
+                            </div>
+                            <div className={cx('main-page__odyssey-actions-item')}>
+                                <img src='https://static.wixstatic.com/media/c794a2_34e01f8153284b68811bd5570305082a~mv2.png/v1/fill/w_163,h_55,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Google_2015_logo_colorless_mourning_period.png' alt='' />
+                            </div>
+                            <div className={cx('main-page__odyssey-actions-item')}>
+                                <img src='https://static.wixstatic.com/media/c794a2_5c7ffbef9b8448d0a20681a7ebff5784~mv2.png/v1/fill/w_190,h_41,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/pngwing_com%20(1).png' alt='' />
+                            </div>
+                            <div className={cx('main-page__odyssey-actions-item')}>
+                                <img src='https://static.wixstatic.com/media/c794a2_5bfd6dbe922a4e5fbc3915deb7201eef~mv2.png/v1/fill/w_170,h_55,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c794a2_5bfd6dbe922a4e5fbc3915deb7201eef~mv2.png' alt='' />
+                            </div>
+                        </div>
+                        <div className={cx('d-flex')}>
+                            <div className={cx('main-page__odyssey-actions-startedLeft')} style={{ border: '1px solid rgb(138, 138, 138)' }}>
+                                {startedVideo && (
+                                    <video autoPlay muted loop>
+                                        <source src="https://video.wixstatic.com/video/c794a2_553d56f3fda64a6bb01d8a7b545f8665/1080p/mp4/file.mp4" type="video/mp4" />
+                                    </video>
+                                )}
+                                <p>Are you ready to transform your business ?</p>
+                            </div>
+                            <div className={cx('main-page__odyssey-actions-startedRight')} style={{ border: '1px solid rgb(138, 138, 138)' }}>
+                                <p>We commit to help you achieve your goals. Whether you're looking to streamline your operations, improve customer engagement, or gain a competitive edge, we've got the tools and expertise you need to succeed</p>
+                                <div>
+                                    <Link to={'/'}>Get Started</Link>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
